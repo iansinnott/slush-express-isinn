c6a65f9f1f75c9620ee5771072099d181de1bd88
Ian Sinnott <ian@iansinnott.com>
2015-01-04 12:20:00 -0800

    Add minification and uglification to build process if in production.

--- templates/gulp/tasks/browserify.js
+++ templates/gulp/tasks/browserify.js
@@ -1,10 +1,17 @@
 var gulp       = require('gulp'),
     browserify = require('browserify'),
-    source     = require('vinyl-source-stream');
+    source     = require('vinyl-source-stream'),
+    buffer     = require('vinyl-buffer'),
+    gulpif     = require('gulp-if'),
+    uglify     = require('gulp-uglify');
+
+var production = process.env.NODE_ENV === 'production';
 
 module.exports = function() {
   browserify(process.cwd() + '/client/')
     .bundle()
-    .pipe(source('app.js'))
+    .pipe(source('app.js')) // Not sure why this AND buffer are both necessary
+    .pipe(buffer()) // Necessary for piping into gulp plugins
+    .pipe(gulpif(production, uglify()))
     .pipe(gulp.dest('public/'));
 };
--- templates/gulp/tasks/styl.js
+++ templates/gulp/tasks/styl.js
@@ -5,7 +5,11 @@ var gulp    = require('gulp'),
     rename  = require('gulp-rename'),
     jeet    = require('jeet'),
     rupture = require('rupture'),
-    nib     = require('nib');
+    nib     = require('nib'),
+    gulpif  = require('gulp-if'),
+    minify  = require('gulp-minify-css');
+
+var production = process.env.NODE_ENV === 'production';
 
 module.exports = function() {
   gulp.src('stylesheets/index.styl')
@@ -13,6 +17,7 @@ module.exports = function() {
     .pipe(stylus({ errors: true, use: [jeet(), nib(), rupture()] }))
     .pipe(prefix({ browsers: ['last 2 versions', 'ie >= 8'] }))
     .pipe(rename('app.css'))
+    .pipe(gulpif(production, minify()))
     .pipe(gulp.dest('public/'));
 };
 
