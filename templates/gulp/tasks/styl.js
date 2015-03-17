e80390c5305213de81afca2cd6b79a509a898b7d
Ian Sinnott <ian@iansinnott.com>
2015-03-16 22:19:00 -0700

    Use axis with stylus

--- templates/gulp/tasks/styl.js
+++ templates/gulp/tasks/styl.js
@@ -1,20 +1,31 @@
-var gulp    = require('gulp'),
-    plumber = require('gulp-plumber'),
-    stylus  = require('gulp-stylus'),
-    prefix  = require('gulp-autoprefixer'),
-    rename  = require('gulp-rename'),
-    jeet    = require('jeet'),
-    rupture = require('rupture'),
-    nib     = require('nib'),
-    gulpif  = require('gulp-if'),
-    minify  = require('gulp-minify-css');
+'use strict';
+
+var gulp        = require('gulp'),
+    plumber     = require('gulp-plumber'),
+    stylus      = require('gulp-stylus'),
+    prefix      = require('gulp-autoprefixer'),
+    rename      = require('gulp-rename'),
+    axis        = require('axis'),
+    typographic = require('typographic'),
+    rupture     = require('rupture'),
+    gulpif      = require('gulp-if'),
+    concat      = require('gulp-concat'),
+    minify      = require('gulp-minify-css'),
+    debug       = require('debug')('gulp:stylus');
+
+var config = require('../../config');
 
 var production = process.env.NODE_ENV === 'production';
 
 module.exports = function() {
-  gulp.src('stylesheets/index.styl')
+  debug('Compiling stylus. File paths: %s, %s', config.paths.stylMain, config.paths.styl);
+
+  // We're using concat b/c we want to make sure our stylus files under
+  // client/lib get concatenated first as they have vars.
+  gulp.src([config.paths.stylMain, config.paths.styl])
     .pipe(plumber())
-    .pipe(stylus({ errors: true, use: [jeet(), nib(), rupture()] }))
+    .pipe(concat('temp.styl'))
+    .pipe(stylus({ errors: true, use: [axis(), rupture(), typographic()] }))
     .pipe(prefix({ browsers: ['last 2 versions', 'ie >= 8'] }))
     .pipe(rename('app.css'))
     .pipe(gulpif(production, minify()))
