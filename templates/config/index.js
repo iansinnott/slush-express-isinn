45f6e9e06f0f3d871b6364984153bd7def43ba5f
Ian Sinnott <ian@iansinnott.com>
2015-03-16 23:00:08 -0700

    Update path cnofiguration

--- templates/config/index.js
+++ templates/config/index.js
@@ -8,10 +8,12 @@ module.exports = {
   version: require('../package.json').version,
 
   // Static asset paths. These are the endpoints for the app's assets
-  path: {
-    css: 'public/app.css',
-    js: 'public/app.js',
-    images: 'public/images/'
+  paths: {
+    js: 'client/**/*.js',                // All JS files
+    jsMain: 'client/index.js',           // App entry point
+    styl: ['client/**/*.styl', 'stylesheets/**/*.styl'], // All stylus files
+    stylMain: 'client/lib/*.styl',       // Main stylus files
+    dest: 'public/'                      // Output dest for js & css
   }
 
 };
